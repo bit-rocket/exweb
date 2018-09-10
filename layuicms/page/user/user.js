56d55d29f05361bc145eacb7d20edd7e997a549a
yeweishuai <ye515430@163.com>
2018-09-10 08:46:25 +0800

    convert to exchange json

--- layuicms/page/user/user.js
+++ layuicms/page/user/user.js
@@ -3,23 +3,23 @@ var $form;
 var form;
 var $;
 layui.config({
-	base : "../../js/"
+    base : "../../js/"
 }).use(['form','layer','upload','laydate'],function(){
-	form = layui.form();
-	var layer = parent.layer === undefined ? layui.layer : parent.layer;
-		$ = layui.jquery;
-		$form = $('form');
-		laydate = layui.laydate;
+    form = layui.form();
+    var layer = parent.layer === undefined ? layui.layer : parent.layer;
+        $ = layui.jquery;
+        $form = $('form');
+        laydate = layui.laydate;
         loadProvince(); //加载省信息
 
     layui.upload({
-    	url : "../../json/userface.json",
-    	success: function(res){
-    		var num = parseInt(4*Math.random());  //生成0-4的随机数
-    		//随机显示一个头像信息
-	    	userFace.src = res.data[num].src;
-	    	window.sessionStorage.setItem('userFace',res.data[num].src);
-	    }
+        url : "../../json/userface.json",
+        success: function(res){
+            var num = parseInt(4*Math.random());  //生成0-4的随机数
+            //随机显示一个头像信息
+            userFace.src = res.data[num].src;
+            window.sessionStorage.setItem('userFace',res.data[num].src);
+        }
     });
 
     //添加验证规则
@@ -87,14 +87,14 @@ layui.config({
 
     //判断是否修改过头像，如果修改过则显示修改后的头像，否则显示默认头像
     if(window.sessionStorage.getItem('userFace')){
-    	$("#userFace").attr("src",window.sessionStorage.getItem('userFace'));
+        $("#userFace").attr("src",window.sessionStorage.getItem('userFace'));
     }else{
-    	$("#userFace").attr("src","../../images/face.jpg");
+        $("#userFace").attr("src","../../images/face.jpg");
     }
 
     //提交个人资料
     form.on("submit(changeUser)",function(data){
-    	var index = layer.msg('提交中，请稍候',{icon: 16,time:false,shade:0.8});
+        var index = layer.msg('提交中，请稍候',{icon: 16,time:false,shade:0.8});
         //将填写的用户信息存到session以便下次调取
         var key,userInfoHtml = '';
         userInfoHtml = {
@@ -118,18 +118,18 @@ layui.config({
             layer.close(index);
             layer.msg("提交成功！");
         },2000);
-    	return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+        return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
     })
 
     //修改密码
     form.on("submit(changePwd)",function(data){
-    	var index = layer.msg('提交中，请稍候',{icon: 16,time:false,shade:0.8});
+        var index = layer.msg('提交中，请稍候',{icon: 16,time:false,shade:0.8});
         setTimeout(function(){
             layer.close(index);
             layer.msg("密码修改成功！");
             $(".pwd").val('');
         },2000);
-    	return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+        return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
     })
 
 })
--- order/order.go
+++ order/order.go
@@ -1,30 +1,29 @@
 package order
 
 import (
-    "fmt"
     "github.com/kataras/iris"
 )
 
 type Users struct {
     UserId      string      `json:"usersId"`
-    UserName    string      `json:"userName"`
-    UserEmail   string      `json:"userEmail"`
-    UserSex     string      `json:"userSex"`
-    UserStatus  string      `json:"userStatus"`
-    UserGrade   string      `json:"userGrade"`
-    UserEndTime string      `json:"userEndTime"`
+    ExName      string      `json:"exName"`
+    TradingPair string      `json:"tradingPair"`
+    Price       string      `json:"price"`
+    Holding     string      `json:"holding"`
+    Earn        string      `json:"earn"`
+    OrderTime   string      `json:"orderTime"`
 }
 
 func ToDealOrders(ctx iris.Context) {
     var user []Users
     peter := Users{
         UserId: "1",
-        UserName: "abc",
-        UserEmail: "abc@xcd.com",
-        UserSex: "male",
-        UserStatus: "buok",
-        UserGrade: "middle",
-        UserEndTime: "2017-05-10 10:30",
+        ExName: "okex",
+        TradingPair: "eos/usdt",
+        Price: "5.01",
+        Holding: "+1.02%",
+        Earn: "20",
+        OrderTime: "2017-05-10 10:30",
     }
     user = append(user, peter)
     ctx.JSON(user)
